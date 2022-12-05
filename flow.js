e254dba2bba87d1abffc6deb3bfcf23c46e22197
zhangsen <1226931269@qq.com>
2022-12-05 20:36:17 +0800

    fix:若 增加返回值

--- flow.js
+++ flow.js
@@ -4,27 +4,29 @@
 
 const 若 = (条件 = false, 动作 = () => { }) => {
     const 条件列表 = []
+    let 其值 = null
     if (条件) {
         条件列表.push(条件)
-        动作()
+        其值 = 动作()
 
     }
     const 反之 = (动作 = () => { }) => {
         const res = 条件列表.indexOf(true)
         if (res === -1) {
-            动作()
+            其值 = 动作()
         }
+        return {其值}
 
 
     }
     const 或若 = (条件 = false, 动作 = () => { }) => {
         if (条件) {
             条件列表.push(条件)
-            动作()
+            其值 = 动作()
         }
-        return { 或若, 反之 }
+        return { 或若, 反之,其值 }
     }
-    return { 反之, 或若 }
+    return { 反之, 或若,其值 }
 
 }
 
